f6c3c00a2cef9382b956f6bbf64d43786fd4b216
Amir Kangarloo <amirkangarloo1051@gmail.com>
2022-03-07 16:14:09 +0330

    Implementation template engine (handlebars)

--- app/bootstrap/index.js
+++ app/bootstrap/index.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const hbs = require('express-handlebars');
+const path = require('path');
+
+module.exports = (app, express) => {
+    app.engine('handlebars', hbs());
+    app.set('view engine', 'handlebars');
+    app.set('views', path.join(__dirname, '../views'));
+    app.use('/static', express.static(path.join(__dirname ,'../../public')));
+};
--- app/index.js
+++ app/index.js
@@ -2,6 +2,14 @@
 
 const express = require('express');
 const app = express();
+require('./bootstrap')(app, express);
+
+app.get('/', (req, res) => {
+    res.render('main', {
+        layout: false,
+        cost: "200$"
+    });
+});
 
 module.exports = () => {
     const port = process.env.APP_PORT;
